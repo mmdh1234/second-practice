6d7d9558bc46bb0f0b2e8db02100517d12235532
hwpark <bhw119@gmail.com>
2023-04-10 09:50:16 +0900

    substract multiply divide

--- calc.py
+++ calc.py
@@ -2,13 +2,19 @@
 # return sum
 def plus(x, y):
     return x+y
+def substract(x, y):
+    return x-y
+def multiply(x, y):
+    return x*y
+def divide(x, y):
+    return x/y
 
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
-    while check >= 1:        
-        print("0: exit, 1: plus")
+    while check >= 1:
+        print("0: exit, 1: plus 2:subtract 3: multiply 4: divide")
         check = int(input())
         if check == 1:
             print("First Number")
@@ -16,6 +22,24 @@ def main():
             print("Second Number")
             y = int(input())
             print("answer : ", plus(x,y))
+        if check == 2:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", substract(x,y))
+        if check == 3:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", multiply(x,y))
+        if check == 4:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", divide(x,y))
         elif check > 1:
             print("Unsupported")
         else:
@@ -23,3 +47,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
